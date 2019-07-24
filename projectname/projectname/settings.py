1ef666865162ee3236ee96450d81cd055dd2c3ee
thisisjasonjafari <jason.jafari.m@gmail.com>
2019-07-25 00:58:54 +0430

    sdfsdf

--- projectname/projectname/settings.py
+++ projectname/projectname/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "app_name.apps.AppNameConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
